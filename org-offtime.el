9be65f08c9626481c818df1067c8a62dbbad3676
Akira Komamura <akira.komamura@gmail.com>
2019-08-25 15:15:11 +0900

    Rewrite; Bump to 0.2
    - Rewrite using org-ql
    - Delete counsel-org-offtime.el and add org-offtime-ivy function

--- org-offtime.el
+++ org-offtime.el
@@ -3,8 +3,8 @@
 ;; Copyright (C) 2018 by Akira Komamura
 
 ;; Author: Akira Komamura <akira.komamura@gmail.com>
-;; Version: 0.1.0
-;; Package-Requires: ((emacs "25.1"))
+;; Version: 0.2
+;; Package-Requires: ((emacs "25.1") (org-ql "0.2"))
 ;; URL: https://github.com/akirak/org-offtime
 
 ;; This file is not part of GNU Emacs.
@@ -41,49 +41,22 @@
 (require 'subr-x)
 (require 'offtime)
 (require 'org)
+(require 'org-ql)
+(require 'org-element)
 
-(defcustom org-offtime-file
-  (expand-file-name "offtime.org" org-directory)
-  "File to track off-time activities."
+(defgroup 'org-offtime nil
+  "Offtime tracking with Org files."
+  :group 'offtime)
+
+(defcustom org-offtime-files nil
+  "List of file names that contain offtime entries."
   :group 'org-offtime
-  :type 'string)
+  :type '(repeat string))
 
-(cl-defun org-offtime--get-headings (&key format-with-schedule)
-  "Generate an alist of headings in `org-offtime-file'.
-
-Each item of the result is a cons cell of its outline path and a marker.
-Scheduled items come first, and then non-scheduled items.
-If FORMAT-WITH-SCHEDULE is non-nil, the schedule is prepended to the path.
-
-Archived items, comment entries, and entries with one of the done states are
-omitted."
-  (with-current-buffer (or (find-buffer-visiting org-offtime-file)
-                           (find-file-noselect org-offtime-file))
-    (setq org-outline-path-cache nil)
-    (mapcar 'cdr
-            (sort (org-with-wide-buffer
-                   (org-map-entries
-                    (lambda ()
-                      (let ((time (org-get-scheduled-time nil)))
-                        (cons time
-                              (cons (concat (when (and format-with-schedule
-                                                       time)
-                                              (format-time-string "%F %R: " time))
-                                            (org-offtime--format-headline))
-                                    (point-marker)))))
-                    nil nil
-                    'archive 'comment '(org-entry-is-done-p)))
-                  (lambda (x y)
-                    (let ((a (car x))
-                          (b (car y)))
-                      (cond
-                       ((and a b) (< (float-time a) (float-time b)))
-                       (a t)
-                       (b nil))))))))
-
-(defun org-offtime--format-headline ()
-  "Format the outline path of the current heading."
-  (org-format-outline-path (org-get-outline-path t t) nil nil "/"))
+(defcustom org-offtime-query '(tag "offtime")
+  "Query to get offtime entries, as supported by org-ql."
+  :group 'org-offtime
+  :type 'sexp)
 
 (defcustom org-offtime-default-action
   'offtime-suspend
@@ -99,66 +72,110 @@ nil."
   :type 'symbol
   :group 'org-offtime)
 
-(defun org-offtime--clock-in (cand &optional func)
-  "Start an off-time clock at CAND and run the default action.
+(defvar org-offtime-history nil)
 
-FUNC is called after clocking in."
-  (let ((marker (cond
-                 ((markerp cand) cand)
-                 ((consp cand) (cdr cand))
-                 ((stringp cand) (org-offtime--create-headline cand)))))
-    (with-current-buffer (marker-buffer marker)
-      (org-with-wide-buffer
-       (goto-char marker)
-       (org-clock-in)))
-    (let ((action (or func org-offtime-default-action)))
-      (when action
-        (funcall action)
-        (when org-offtime-clock-out
-          (org-clock-out))))))
+(defun org-offtime-get-candidates ()
+  "Get offtime entries."
+  (mapcar (lambda (element)
+            (let ((marker (org-element-property :org-hd-marker element)))
+              (with-current-buffer (marker-buffer marker)
+                (org-with-wide-buffer
+                 (goto-char (marker-position marker))
+                 (org-offtime--format-headline)
+                 (propertize (org-offtime--format-entry)
+                             'org-hd-marker marker)))))
+          (org-ql-select org-offtime-files org-offtime-query
+            :action 'element-with-markers)))
+
+(defun org-offtime--format-entry ()
+  "Format the current entry."
+  (format "%s: %s"
+          (buffer-name (org-base-buffer (current-buffer)))
+          (org-format-outline-path (org-get-outline-path t t))))
+
+(defun org-offtime--select ()
+  "Return a marker to an entry selected by `org-offtime-get-candidates'."
+  (let ((ent (completing-read "Offtime entry: "
+                              (org-offtime-get-candidates)
+                              nil t nil org-offtime-history)))
+    (get-text-property 0 'org-hd-marker ent)))
+
 ;;;###autoload
+(cl-defun org-offtime-clock-in (marker &optional (func org-offtime-default-action))
+  "Clock in to an entry at MARKER with FUNC called afterwards."
+  (interactive (list (org-offtime--select)))
+  (with-current-buffer (marker-buffer marker)
+    (unless (derived-mode-p 'org-mode)
+      (error "Not in org mode"))
+    (org-with-wide-buffer
+     (message "org-offtime: Clock in at %s to %s"
+              (org-timestamp-format
+               (org-timestamp-from-time (current-time) t t)
+               (org-time-stamp-format t t))
+              (org-offtime--format-entry))
+     (goto-char marker)
+     (org-clock-in)))
+  (when func
+    ;; Await for the function to finish
+    (funcall func)
+    (when org-offtime-clock-out
+      (message "org-offtime: Clock out at %s"
+               (org-timestamp-format
+                (org-timestamp-from-time (current-time) t t)
+                (org-time-stamp-format t t)))
+      (org-clock-out))))
 
-(defun org-offtime-clock-in ()
+;;;###autoload
+(defun org-offtime-clock-in-this-entry ()
   "Start an off-time clock on the current headline in org mode."
   (interactive)
   (unless (derived-mode-p 'org-mode)
     (error "Not in org-mode"))
-  (org-offtime--clock-in (point-marker)))
+  (org-offtime-clock-in (point-marker)))
+
+;; (defun org-offtime--create-headline (path)
+;;   "Create a headline from PATH."
+;;   (with-current-buffer (or (find-buffer-visiting org-offtime-file)
+;;                            (find-file-noselect org-offtime-file))
+;;     (org-with-wide-buffer
+;;      (goto-char (point-min))
+;;      (cl-loop for heading in (split-string path "/")
+;;               do (let* ((level (if (= (point) (point-min))
+;;                                    0
+;;                                  (nth 0 (org-heading-components))))
+;;                         (next-level (org-get-valid-level (1+ level)))
+;;                         (subtree-end (if (= level 0)
+;;                                          (point-max)
+;;                                        (save-excursion (org-end-of-subtree)))))
+;;                    (while (or (= (point) (point-min))
+;;                               (not (= next-level (nth 0 (org-heading-components)))))
+;;                      (unless (re-search-forward
+;;                               (format org-complex-heading-regexp-format
+;;                                       (regexp-quote heading))
+;;                               subtree-end t)
+;;                        (goto-char subtree-end)
+;;                        (insert "\n" (make-string next-level ?\*) " " heading))))
+;;               finally return (point-marker)))))
 
 ;;;###autoload
-(defun org-offtime (&optional func)
-  "Choose an off-time state from org-offtime-file.
-
-FUNC is a function used to enter the off-time state, e.g. `offtime-suspend'."
-  (interactive)
-  (let* ((cands (org-offtime--get-headings :format-with-schedule t))
-         (cand (completing-read "off-time: " cands)))
-    (when cand
-      (org-offtime--clock-in (or (assoc cand cands) cand) func))))
-
-(defun org-offtime--create-headline (path)
-  "Create a headline from PATH."
-  (with-current-buffer (or (find-buffer-visiting org-offtime-file)
-                           (find-file-noselect org-offtime-file))
-    (org-with-wide-buffer
-     (goto-char (point-min))
-     (cl-loop for heading in (split-string path "/")
-              do (let* ((level (if (= (point) (point-min))
-                                   0
-                                 (nth 0 (org-heading-components))))
-                        (next-level (org-get-valid-level (1+ level)))
-                        (subtree-end (if (= level 0)
-                                         (point-max)
-                                       (save-excursion (org-end-of-subtree)))))
-                   (while (or (= (point) (point-min))
-                              (not (= next-level (nth 0 (org-heading-components)))))
-                     (unless (re-search-forward
-                              (format org-complex-heading-regexp-format
-                                      (regexp-quote heading))
-                              subtree-end t)
-                       (goto-char subtree-end)
-                       (insert "\n" (make-string next-level ?\*) " " heading))))
-              finally return (point-marker)))))
+(with-eval-after-load 'ivy
+  ;; TODO: Might need to declare ivy-read
+  (defun org-offtime-ivy ()
+    "Use Ivy to enter off-time."
+    (interactive)
+    (ivy-read "off-time: " (org-offtime--select)
+              :require-match t
+              :caller 'counsel-org-offtime
+              :action '(1 ("o" org-offtime-clock-in "default")
+                          ("s"
+                           (lambda (marker)
+                             (org-offtime-clock-in marker 'offtime-suspend))
+                           "suspend")
+                          ("l"
+                           (lambda (marker)
+                             (org-offtime-clock-in marker 'offtime-lock))
+                           "lock")
+                          ("g" #'org-goto-marker-or-bmk "go to the heading")))))
 
 (provide 'org-offtime)
 ;;; org-offtime.el ends here
